73e6d45ee5ea60a8285f5209e91a72e723267336
Mateus Garcia <74436309+mat-garcia@users.noreply.github.com>
2021-06-15 20:16:05 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 try{
-$pdo = new PDO ("pgsql:host=localhost;dbname=mydata","postgres","theminecs2");
+$pdo = new PDO ("pgsql:host=ec2-54-160-7-200.compute-1.amazonaws.com;dbname=dl2l9b69759n2","ebmvowswhkvuqi","8920718ceeb416497dcb004debd646f871be38ce45effb4aa2ec78a2cd0a951b");
 
 } catch(PDOException $e){
-    
+    echo ("is Done!")
 }
 
 ?>

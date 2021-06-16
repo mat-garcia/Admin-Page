0da3ade700037718487c0f2c9a5ed97d17d58e7e
Mateus Garcia <74436309+mat-garcia@users.noreply.github.com>
2021-06-15 23:02:03 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
-
 </body>
 <!-- Scripts files-->
 <script type="text/javascript" src="./js/materialize.js"></script>
 <script type="text/javascript" src="./js/jquery.min.js"></script>
 <script type="text/javascript" src="./js/via.js"></script>
-
-
 </html>

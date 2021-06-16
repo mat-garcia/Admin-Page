04fd7bef2a261a882d8af2cc771149ad3c675aeb
Mateus Garcia <74436309+mat-garcia@users.noreply.github.com>
2021-06-16 00:27:34 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,6 @@
 <?php
 require "db.php";
+$pdo = new PDO ("pgsql:host=ec2-54-160-7-200.compute-1.amazonaws.com;port=5432;dbname=dl2l9b69759n2","ebmvowswhkvuqi","8920718ceeb416497dcb004debd646f871be38ce45effb4aa2ec78a2cd0a951b");
 $id = $_GET['id'];
 $sql = "DELETE FROM clientes WHERE id=:id";
 $stmt = $pdo->prepare($sql);

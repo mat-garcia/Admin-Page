7eb5d7d53e352f7f3663f7c4aa12ba6c6c1c47ff
Mateus Garcia <74436309+mat-garcia@users.noreply.github.com>
2021-06-16 00:22:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php require'db.php';
+<?php require 'db.php';
+$pdo = new PDO ("pgsql:host=ec2-54-160-7-200.compute-1.amazonaws.com;port=5432;dbname=dl2l9b69759n2","ebmvowswhkvuqi","8920718ceeb416497dcb004debd646f871be38ce45effb4aa2ec78a2cd0a951b");
 $sql = 'SELECT * FROM clientes';
 $stmt = $pdo->prepare($sql);
 $stmt->execute();

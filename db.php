30f7f9567aae8cc71f3e2e5e4da658a989da7b56
Mateus Garcia <74436309+mat-garcia@users.noreply.github.com>
2021-06-15 21:18:35 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,10 @@
 <?php
 try{
-$pdo = new PDO ("pgsql:host=ec2-54-160-7-200.compute-1.amazonaws.com;port=5432;dbname=dl2l9b69759n2","ebmvowswhkvuqi","8920718ceeb416497dcb004debd646f871be38ce45effb4aa2ec78a2cd0a951b");
+$pdo = new PDO ("pgsql:host=ec2-54-160-7-200.compute-1.amazonaws.com;port=5432;dbname=dl2l9b69759n2","ebmvowswhkvuqi
+","8920718ceeb416497dcb004debd646f871be38ce45effb4aa2ec78a2cd0a951b");
 
 } catch(PDOException $e){
-    echo ("is Done!")
+    
 }
 
 ?>
